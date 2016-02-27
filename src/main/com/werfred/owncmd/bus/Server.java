e56dea41b9cf36cb539a458b1d534fd9511f10aa
Diego Savardi <diego@snowyowl.fritz.box>
2016-02-28 00:47:42 +0100

    Sent first message on bus. Works.

--- src/main/com/werfred/owncmd/bus/Server.java
+++ src/main/com/werfred/owncmd/bus/Server.java
@@ -1,49 +1,119 @@
 package com.werfred.owncmd.bus;
 
-import com.werfred.owncmd.messages.SystemMessage;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.InetAddress;
 import java.net.Socket;
+import java.util.logging.Logger;
+
+import com.werfred.owncmd.messages.SystemMessage;
 
 /**
  * Created by diego on 26/02/16.
  */
 public class Server {
-    private String address;
-    private int port;
+	private Logger logger;
+	private String address;
+	private int port;
+	private BufferedReader in;
+	private PrintWriter out ;
+	private Socket socket;
 
-    public Server(String address, int port) {
-        this.address = address;
-        this.port = port;
-    }
+	public Server(String address, int port) throws Exception {
+		this.logger = Logger.getLogger(Server.class.getName());
+		this.logger.info("New Server address:" + address + " port:" + port);
+		this.address = address;
+		this.port = port;
+	}
 
-    public boolean sendCommand(String action) throws Exception {
-        Socket socket = this.openSocket(this.address, this.port);
+	public boolean sendCommand(String message) throws Exception {
+		this.logger.info("sendCommand message:" + message);
+		boolean connectionOpened = this.openConnection(this.address, this.port);
+		this.sendMessage(SystemMessage.OPEN_COMMAND_SESSION, this.out);
+		this.checkACK(this.in);
+	    this.sendMessage(message, this.out);
+	    boolean messageSent = this.checkACK(this.in);
+		this.closeConnection();
+		return messageSent;
+	}
+	
+	public String getStatus(String message) throws Exception {
+		this.logger.info("sendCommand message:" + message);
+		boolean connectionOpened = this.openConnection(this.address, this.port);
+		this.sendMessage(SystemMessage.OPEN_COMMAND_SESSION, this.out);
+		this.checkACK(this.in);
+	    this.sendMessage(message, this.out);
+	    String status = this.readMessage(this.in);
+	    this.checkACK(this.in);
+		this.closeConnection();
+		return status;
+	}
 
-        BufferedReader inputStream = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        PrintWriter outputStream = new PrintWriter(socket.getOutputStream(),true);
+	private void closeConnection() throws IOException {
+		this.logger.info("closeConnection");
+		this.in.close();
+		this.in = null;
+		this.out.close();
+		this.out = null;
+		this.socket.close();
+		this.socket = null;
+	}
+	
+	private boolean openConnection(String ip, int port) throws Exception {
+		this.logger.info("openConnection ip:" + ip + " port:" + port);
+		this.socket = new Socket(ip, port);
+		this.out = new PrintWriter(socket.getOutputStream(), true);
+		this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		return this.checkACK(this.in);
+	}
+	
+	private void sendMessage(String message, PrintWriter writer) throws Exception {
+		this.logger.info("sendCommand message:" + message);
+		writer.write(message);
+		writer.flush();
+	}
+	
+	private boolean checkACK(BufferedReader reader) throws IOException {
+		this.logger.info("checkACK");
+		boolean response = false;
+		String message = this.readMessage(reader);
+		if (message.equals(SystemMessage.ACK)) {
+			response = true;
+		}
+		return response;
+	}
 
-
-        outputStream.write(SystemMessage.OPEN_EVENT_SESSION);
-        outputStream.flush();
-        return true;
-
-    }
-
-    public boolean readEvent(String action) {
-        return false;
-    }
-
-
-
-    private Socket openSocket(String address, int port) throws Exception {
-        InetAddress ip = InetAddress.getByName(address);
-        Socket socket = socket = new Socket(ip, port);
-        return socket;
-    }
+	private String readMessage(BufferedReader reader) throws IOException {
+		StringBuffer message = new StringBuffer();
+		int charInt = 0;
+		char c = ' ';
+		boolean foundFirst = false;
+		boolean foundSecond = false;
+		// Message finish with '##'
+		int cycle = 0;
+		do {
+			cycle ++;
+			charInt = reader.read();
+			if (charInt == -1) {
+				this.logger.info("Premature end of stream.");
+				throw new IOException("Premature end of stream from OWN Server.");
+			} else {
+				c = (char) charInt;
+				message.append(c);
+				if (c == '#') { // Found first #
+					if (foundFirst) {
+						foundSecond = true;
+					}
+					foundFirst = true;
+				} else {
+					foundFirst = false;
+				}
+			}
+		} while (!foundSecond && cycle < 100);
+		String msg = message.toString();
+		this.logger.info("readMessage message is: " + msg);
+		return msg;
+	}
 
 }
--- src/test/com/werfred/owncmd/bus/ServerTest.java
+++ src/test/com/werfred/owncmd/bus/ServerTest.java
@@ -4,15 +4,28 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import org.junit.Assert;
+
 /**
  * Created by diego on 26/02/16.
  */
 public class ServerTest {
+	//START command line tool "netcat -L 192.168.28.35 -p 20000"
 
     @Test
     public void testSendCommand() throws Exception {
     	System.out.println("Start");
         Server server = new Server("192.168.28.35", 20000);
-        server.sendCommand("ciao");
+        boolean execResult = server.sendCommand("*1*12*13##");
+        Assert.assertTrue(execResult);
+    }
+    
+    @Test
+    public void testGettatus() throws Exception {
+    	System.out.println("Start");
+        Server server = new Server("192.168.28.35", 20000);
+        String status = server.getStatus("*#1*13##");
+        System.out.println("Status: " + status);
+  
     }
 }
